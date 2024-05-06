7366230b65dcee1c37a026ce4813d2c83d57771c
Ahmedbenhassouna1 <ahmedbnh01@gmail.com>
2024-05-06 23:19:58 +0100

    update project

--- src/test/java/com/todos/tests/AddTodosTest.java
+++ src/test/java/com/todos/tests/AddTodosTest.java
@@ -1,5 +1,25 @@
 package com.todos.tests;
 
-public class AddTodosTest {
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.todos.page.TodosPage;
+import com.todos.utils.Setup;
+
+public class AddTodosTest extends Setup {
+
+	public AddTodosTest() throws IOException {
+		super();
+	}
+	
+	TodosPage todoPage;
+	
+	@Test
+	public void iCanAddTodos() throws IOException {
+		
+		todoPage = new TodosPage();
+		todoPage.submitTodo(prop.getProperty("todo1"));
+	}
 
 }
